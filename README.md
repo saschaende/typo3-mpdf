fa3ba9080cc32719e62b16b07c29dd2326469ef3
Sascha Ende <sascha@ende.us>
2018-07-24 09:52:47 +0200

    Modified   README.md Add        ext_emconf.php Add        ext_localconf.php

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # typo3-mpdf
-MPDF library
+MPDF library for TYPO3
+
+mPDF is a PHP library which generates PDF files from UTF-8 encoded HTML. It is based on FPDF and HTML2FPDF with a number of enhancements. More: https://mpdf.github.io/
---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'MPDF',
+    'description' => 'mPDF is a PHP library which generates PDF files from UTF-8 encoded HTML. It is based on FPDF and HTML2FPDF with a number of enhancements. More: https://mpdf.github.io/',
+    'category' => 'tools',
+    'author' => 'Sascha Ende',
+    'author_email' => 'sascha@sascha-ende.de',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '7.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.6.0-8.7.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];

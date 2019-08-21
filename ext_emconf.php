df07a43f7439a680a0613fce794f33fb7364f2df
Sascha Ende <sascha@ende.us>
2019-08-21 11:58:00 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '8.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.6.0-9.5.5',
+            'typo3' => '8.6.0-9.9.9',
         ],
         'conflicts' => [],
         'suggests' => [],
